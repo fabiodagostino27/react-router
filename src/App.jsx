533bdd9621f9925f7890db3dbef66883a6f3787f
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-28 11:56:30 +0100

    ADD: homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 
 function App() {
 
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" Component={HomePage} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
