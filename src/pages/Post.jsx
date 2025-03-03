df9f1fc78a03dd6b36b2469476a8b03da7ab4e9f
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-03 12:45:35 +0100

    ADD: navigation between posts

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,28 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Post() {
     const {id} = useParams()
+    const [post, setPost] = useState({});
+
+    const fetchPost = () => {
+        fetch(`http://localhost:3000/posts/api/${id}`)
+            .then((res) => res.json())
+            .then(data => setPost(data))
+    }
+
+    const navigate = useNavigate()
+
+    useEffect(fetchPost, [id])
 
     return (
-        <h1>Pagina post: {id}</h1>
+        <div className="container">
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <div className="d-flex gap-2 justify-content-center">
+                <button className="btn btn-primary" onClick={() => navigate(`/posts/${parseInt(id) - 1}`)}>Vai al post precedente</button>
+                <button className="btn btn-primary" onClick={() => navigate(`/posts/${parseInt(id) + 1}`)}>Vai al post successivo</button>
+            </div>
+        </div>
     )
 }
