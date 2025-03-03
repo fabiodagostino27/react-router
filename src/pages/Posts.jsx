71a88879144f43c618de89badcc284cf5f72c4ab
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-03 12:17:26 +0100

    ADD: posts list

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
+
 export default function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = () => {
+        fetch("http://localhost:3000/posts/api")
+            .then((res) => res.json())
+            .then(data => setPosts(data))
+    }
+
+    useEffect(fetchPosts, []);
+
     return (
         <div className="container">
-            <h1>Lista dei post</h1>
+            <h1 className="mb-4">Lista dei post</h1>
+            <div className="row row-cols-3">
+                {
+                    posts.map(p => {
+                        return (
+                            <div key={p.id} className="card col" >
+                                <div className="card-body">
+                                    <h5 className="card-title">{p.title}</h5>
+                                    <p className="card-text">{p.content}</p>
+                                    <a href="#" className="btn btn-primary align-self-end">Apri Post</a>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
